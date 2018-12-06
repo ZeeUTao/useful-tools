33fcd5b9e0664869d3d0d66c040672ce3c18679f
ZeeUTao <39596110+ZeeUTao@users.noreply.github.com>
2018-12-06 15:15:11 +0800

    Add files via upload

---
+++ peach.py
@@ -0,0 +1,21 @@
+import numpy as np
+from qutip import *
+import qutip.tensor as t
+import matplotlib.pyplot as plt
+# from picture import *
+
+
+x, y, z, i = sigmax(),sigmay(),sigmaz(),qeye(2)
+
+
+def qpt(gate = sigmax()):
+	q_num = gate.shape[0] / 2
+	U_rho = spre(gate) * spost(gate.dag())
+	op_basis = [[qeye(2), sigmax(), sigmay(), sigmaz()]] * q_num
+	op_label = [["i", "x", "y", "z"]] * q_num
+	chi = qutip.qpt(U_rho, op_basis)
+	fig = qpt_plot_combined(chi, op_label)
+	return Qobj(chi)
+	
+def draw_mat(mat):
+	matrix_histogram(Qobj(mat))
