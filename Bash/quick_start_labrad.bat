c6c89ef409f9d4f06b7c41271d44342d7199d2c4
ZeeUTao <39596110+ZeeUTao@users.noreply.github.com>
2021-07-01 17:19:01 +0800

    Create quick_start_labrad.bat

---
+++ Bash/quick_start_labrad.bat
@@ -0,0 +1,10 @@
+
+start cmd /k python N:\servers\data_vault.py
+start cmd /k N:\servers\directethernet.exe
+start cmd /k python "N:\servers\GHz DACs\ghz_fpga_server.py"
+start cmd /k python N:\servers\gpib_device_manager.py
+start cmd /k python N:\servers\gpib_server.py
+start cmd /k python N:\servers\Anritsu\anritsu.py
+start cmd /k python N:\servers\Anritsu\Hittite_T2100.py
+start cmd /k python N:\servers\dac_calibration_server.py
+start cmd /k D:\Software\Java\jre1.8.0_162\bin\java -jar N:\servers\Qubit_Server\QubitServer.jar
